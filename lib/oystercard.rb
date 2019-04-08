7facad018f244b012db7267ca2aeba2e0d54fe05
Alasdair Wem <alasdair123@hotmail.co.uk>
2019-04-08 15:49:46 +0100

    add balance? method to oystercard

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,2 +1,5 @@
 class Oystercard
+  def balance?
+    0
+  end 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,10 @@
 require 'oystercard'
 
 describe Oystercard do
+
+  describe '#balance?' do
+    it 'displays balance on oystercard' do
+      expect(subject.balance?).to eq 0 
+    end
+  end
 end
